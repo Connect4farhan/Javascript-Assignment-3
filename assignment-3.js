e57c5e14226ec1ffd48c7723fb120e58987ae2d6
Farhan Ahmed <153209469+Connect4farhan@users.noreply.github.com>
2024-02-05 16:22:33 +0500

    Add files via upload

---
+++ assignment-3.js
@@ -0,0 +1,140 @@
+
+// Q-1
+// var city = prompt("Enter the city name: ");
+
+// if (city.toLowerCase() === "karachi") {
+//     alert("Welcome to the city of lights");
+// }
+
+
+// Q-2
+// var gender = prompt("Enter your gender : ");
+// if (gender.toLowerCase() === "male") {
+//     alert("Good Morning Sir ");
+// } else if (gender.toLowerCase() === "female") {
+//     alert("Good Morning Maam ");
+// } 
+
+// Q-3
+// var fuel = +prompt("Enter the remaining fuel in your car in litres: ");
+// if (fuel < 0.25) {
+//     alert("Please refill the fuel in your car.");
+// } else {
+//     alert("Refilling fuel is not necssary");
+// }
+
+
+// Q-4
+
+// var a = 4;
+// if (++a === 5) {
+//     alert("given condition for variable a is true");
+// }
+
+
+// var b = 82;
+// if (b++ === 83) {
+//     alert("given condition for variable b is true");
+// }
+
+
+// var c = 12;
+// if (c++ === 13) {
+//     alert("condition 1 is true");
+// }
+// if (c === 13) {
+//     alert("condition 2 is true");
+// }
+// if (++c < 14) {
+//     alert("condition 3 is true");
+// }
+// if (c === 14) {
+//     alert("condition 4 is true");
+// }
+
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost) {
+//     alert("The cost equals");
+// }
+
+
+// if (true) {
+//     alert("True");
+// }
+// if (false) {
+//     alert("False");
+// }
+
+
+// if ("car" < "cat") {
+//     alert("car is smaller than cat");
+// }
+
+
+// Q-5
+// var secretNum = 3;
+// var user = +prompt("Guess the secret number (between 1 to 10): ");
+// if (user === secretNum) {
+//     alert("Great! its correct answer.");
+// } else if (user + 1 === secretNum || user - 1 === secretNum) {
+//     alert("Close enough to the correct answer.");
+// } else {
+//     alert("Sorry, incorrect guess")
+// }
+
+// Q-6
+// var user = +prompt("Enter a number: ");
+// if (user % 3 == 0) {
+//     alert(user + " is divisible by 3.");
+// } else {
+//     alert(user + " is not divisible by 3.");
+// }
+
+// Q-7
+// var user = +prompt("Enter a number: ");
+// if (user % 2 === 0) {
+//     alert(user + " is an even number.");
+// } else {
+//     alert(user + " is an odd number.");
+// }
+
+
+// Q-8
+// var temp = +prompt("Enter the temperature: ");
+// if (temp > 40) {
+//     alert("It is too hot outside.");
+// } else if (temp > 30) {
+//     alert("The Weather today is Normal.");
+// } else if (temp > 20) {
+//     alert("Today’s Weather is cool.");
+// } else if (temp > 10) {
+//     alert("OMG! Today’s weather is so Cool.");
+// } else {
+//     alert("It's very cold outside.");
+// }
+
+
+// Q-9
+// var num1 = +prompt("Enter the first number: ");
+// var num2 = +prompt("Enter the second number: ");
+// var perform = prompt("Enter the operation (+, -, *, /, %): ");
+// var result;
+// if (perform === "+") {
+//     result = num1 + num2;
+// } else if (perform === "-") {
+//     result = num1 - num2;
+// } else if (perform === "*") {
+//     result = num1 * num2;
+// } else if (perform === "/") {
+//     result = num1 / num2;
+// } else if (perform === "%") {
+//     result = num1 % num2;
+// } else {
+//     alert("Invalid operation");
+// }
+// alert("Result: " + result);
+
+
